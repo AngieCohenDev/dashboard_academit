f98c9d26f6d39673f4e1e159c5db3cd6ffdc1139
Angie Cohen <angiedev2001@gmail.com>
2024-05-14 16:05:34 -0300

    add: Creacion y estructura del componente BuyerProfile

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,3 +1,4 @@
+import BuyerProfileChart from "./BuyerProfileChart";
 import DashboardStatsGrid from "./DashboardStatsGrid";
 import TransactionChart from "./TransactionChart";
 
@@ -5,7 +6,10 @@ export default function Dashboard() {
   return (
     <div className="flexflex-col gap-4">
       <DashboardStatsGrid />
-      <TransactionChart />
+      <div className="flex flex-row gap-4 w-full mt-2">
+        <TransactionChart />
+        <BuyerProfileChart />
+      </div>
     </div>
   );
 }
