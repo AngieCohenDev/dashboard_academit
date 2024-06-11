fdab406fc5e0b0923af0713828392eec4e2294ab
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-11 15:50:34 -0300

    creado el hook useOnSubmitNewArticle.js para manejar toda la logica de js del componente NewArticleForm.jsx

--- src/components/NewArticleForm.jsx
+++ src/components/NewArticleForm.jsx
@@ -1,17 +1,21 @@
 import classNames from "classnames";
 import { useForm } from "react-hook-form";
 import { postNewArticle } from "../helpers/fetchNewArticle";
+import { useOnSubmitNewArticle } from "./hooks/useOnSubmitNewArticle";
 
 const styleLabel = "font-medium text-sm py-1 ";
 const styleInput = "w-full h-[40px] px-2 text-slate-400 text-xs my-1 rounded-lg border bg-gray-100";
 
 export default function NewArticleForm() {
-  const { register, handleSubmit, reset, } = useForm();
 
-  const onSubmit = handleSubmit((data) => {
-    postNewArticle(data);
-    reset();
-  });
+  const { onSubmit, register } = useOnSubmitNewArticle()
+
+  // const { register, handleSubmit, reset, } = useForm();
+
+  // const onSubmit = handleSubmit((data) => {
+  //   postNewArticle(data);
+  //   reset();
+  // });
 
   return (
     <div className="flex justify-center min-h-screen bg-gray-100">
@@ -23,41 +27,41 @@ export default function NewArticleForm() {
 
           {/* Título de la sección */}
           <div className="my-2">
-          <label htmlFor="title_seccion" className={classNames(styleLabel)}>
-            Ingrese el título de la sección
-          </label>
-          <input
-            className={classNames(styleInput)}
-            type="text"
-            placeholder="Por favor ingrese el título de la sección"
-            {...register("sectiontitle")}
-          />
+            <label htmlFor="title_seccion" className={classNames(styleLabel)}>
+              Ingrese el título de la sección
+            </label>
+            <input
+              className={classNames(styleInput)}
+              type="text"
+              placeholder="Por favor ingrese el título de la sección"
+              {...register("sectiontitle")}
+            />
           </div>
 
           {/* Título del artículo */}
           <div className="my-2">
-          <label htmlFor="title_article" className={classNames(styleLabel)}>
-            Ingrese el título del artículo
-          </label>
-          <input
-            className={classNames(styleInput)}
-            type="text"
-            placeholder="Por favor ingrese el título del artículo"
-            {...register("articletitle")}
-          />
+            <label htmlFor="title_article" className={classNames(styleLabel)}>
+              Ingrese el título del artículo
+            </label>
+            <input
+              className={classNames(styleInput)}
+              type="text"
+              placeholder="Por favor ingrese el título del artículo"
+              {...register("articletitle")}
+            />
           </div>
 
           {/* Descripcion */}
           <div className="my-2">
-          <label htmlFor="descripcion" className={classNames(styleLabel)}>
-            Ingrese la descripción
-          </label>
-          <input
-            className={classNames(styleInput)}
-            type="text"
-            placeholder="Por favor ingrese la descripción"
-            {...register("description")}
-          />
+            <label htmlFor="descripcion" className={classNames(styleLabel)}>
+              Ingrese la descripción
+            </label>
+            <input
+              className={classNames(styleInput)}
+              type="text"
+              placeholder="Por favor ingrese la descripción"
+              {...register("description")}
+            />
           </div>
 
           <button
