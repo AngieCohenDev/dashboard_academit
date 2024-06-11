3eb974e3f7f90d9557fb42629e6e6932cbdf677a
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-11 15:49:42 -0300

    modificado archivo solicitudesFetch para recibir ditintos parametros y segun estos realizar una solicitud especifica

--- src/helpers/solicitudesFetch.js
+++ src/helpers/solicitudesFetch.js
@@ -1,32 +1,56 @@
 // postRequest.js
 
 // Función para realizar una solicitud POST a un endpoint usando fetch
-export const solicitudPost = async (formData, param) => {
+export const solicitudFetch = async (requestData, method, param, id) => {
     try {
 
-        // Realiza la solicitud POST al endpoint especificado
-        const response = await fetch(`http://localhost:8080/${param}`, {
-            method: 'POST',
-            body: formData
-        });
+        let options = {
+            method
+        };
 
-        // Verifica si la respuesta fue exitosa
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.status}`);
+        if(!requestData) {
+            const response = await fetch(`http://localhost:8080/${param}`, options);
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.status}`);
+            }
+            const data = await response.json();
+            return data[0];
         }
 
-        // Analiza la respuesta como JSON
-        const data = await response.json();
+        // Verificar si data es una instancia de FormData
+        if (requestData instanceof FormData) {
+            options.body = requestData; // Pasar FormData directamente
+            // No es necesario establecer el encabezado Content-Type
+        } else {
+            options.headers = {
+                'Content-Type': 'application/json'
+            };
+            options.body = JSON.stringify(requestData); // Convertir data a JSON
+        }
 
-        // Muestra los datos en la consola
-        console.log('Datos recibidos:', data);
+        if (id) {
+            const response = await fetch(`http://localhost:8080/${param}/${id}`, options);
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log('Datos recibidos:', data);
+            return data[0];
+        } else {
+            const response = await fetch(`http://localhost:8080/${param}`, options);
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log('Datos recibidos:', data);
+        }
     } catch (error) {
         // Manejo de errores
         console.error('Error al realizar la solicitud:', error);
     }
 }
 
-export const solicitudPatch = async (formData , param,id) => {
+export const solicitudPatch = async (formData, param, id) => {
     try {
 
         // Realiza la solicitud PATCH al endpoint especificado
