823c9ba0737117a8ef140394603a14a6e6b8e6ec
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-14 20:16:42 -0300

    modificado componente MainArticleForm.jsx para manejar el formulario desde un boton y mostrar los datos en una tabla

--- src/components/MainArticleForm.jsx
+++ src/components/MainArticleForm.jsx
@@ -1,77 +1,139 @@
 import classNames from "classnames";
 import ImageUpload from "../helpers/ImageUpload";
 import { useOnSubmitMainArticle } from "./hooks/useOnSubmitMainArticle";
+import { useState } from "react";
+import { Table } from "./shared/Table";
+import axios from "axios";
+
+const articlesField = {
+  keys: ["id", "title", "description", "textButton", "image", "createdAt", "updatedAt"],
+  labels: ["Id", "Titulo", "Descripción", "Texto Del Botón", "Imagen", "Creado", "Actualizado"],
+};
 
 const styleLabel = "font-medium text-sm py-1 ";
 const styleInput = "w-full h-[40px] px-2 text-slate-400 text-xs my-1 rounded-lg border bg-gray-100";
 
+const callApi = async () => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "http://localhost:8080/main-article",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  const response = await axios.request(config);
+  console.log(JSON.stringify(response.data, null, 4));
+  return response.data;
+};
+
 export default function MainArticleForm() {
-  
-  const { handleFileChange, onSubmit, register} = useOnSubmitMainArticle()
+
+  const { handleFileChange, onSubmit, register } = useOnSubmitMainArticle()
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
-    <div className="flex justify-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-md rounded-lg p-8 w-2/5 h-3/5 mx-4">
-        <div className="mb-6">
-          <h1 className="text-2xl flex items-center justify-center font-bold">Formulario main article</h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      {/* Botón para abrir el popup */}
+      <div className="flex justify-between items-center px-4 py-2 bg-white shadow-md mb-4">
+        <input
+          type="text"
+          className="w-1/3 h-10 px-2 text-slate-400 text-sm my-1 rounded-lg border bg-gray-100"
+          placeholder="Buscar..."
+        />
+        <button
+          onClick={openPopup}
+          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline"
+        >
+          Abrir Formulario
+        </button>
+      </div>
+      {/* Popup */}
+      {showPopup && (
+        <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full bg-gray-800 bg-opacity-75">
+          <div className="bg-white shadow-md rounded-lg p-8 w-2/3">
+            <div className="flex justify-between items-center mb-6">
+              <h1 className="text-2xl font-bold">Formulario de Artículo</h1>
+              <button
+                onClick={closePopup}
+                className="text-gray-600 hover:text-gray-700 focus:outline-none"
+              >
+                X
+              </button>
+            </div>
+            <form onSubmit={onSubmit} className="flex flex-col font-sans">
+              <div className="my-2">
+                {/* Titulo */}
+                <label htmlFor="title" className={classNames(styleLabel)}>
+                  Ingrese el título
+                </label>
+                <input
+                  className={classNames(styleInput)}
+                  type="text"
+                  placeholder="Por favor ingrese el título"
+                  {...register("title")}
+                />
+              </div>
+
+              <div className="my-2">
+                {/* Descripcion */}
+                <label htmlFor="descripcion" className={classNames(styleLabel)}>
+                  Ingrese la descripción
+                </label>
+                <input
+                  className={classNames(styleInput)}
+                  type="text"
+                  placeholder="Por favor ingrese la descripción"
+                  {...register("description")}
+                />
+              </div>
+
+              <div className="my-2">
+                {/* Texto del botón */}
+                <label htmlFor="text-btn" className={classNames(styleLabel)}>
+                  Ingrese el texto para el botón
+                </label>
+                <input
+                  className={classNames(styleInput)}
+                  type="text"
+                  placeholder="Por favor ingrese el texto del botón"
+                  {...register("textButton")}
+                />
+              </div>
+
+              <div className="my-2">
+                {/* Fondo */}
+                <label htmlFor="img" className={classNames(styleLabel)}>
+                  Seleccione el fondo
+                </label>
+                <ImageUpload onFileChange={handleFileChange} />
+              </div>
+
+              <div className="flex justify-end mt-8">
+                <button
+                  type="submit"
+                  className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline"
+                >
+                  Enviar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <form onSubmit={onSubmit} className="flex flex-col font-sans">
+      )}
 
-          <div className="my-2">
-            {/* Titulo */}
-            <label htmlFor="title" className={classNames(styleLabel)}>
-              Ingrese el titulo
-            </label>
-            <input
-              className={classNames(styleInput)}
-              type="text"
-              placeholder="Por favor ingrese el titulo"
-              {...register("title")}
-            />
-          </div>
-
-          <div className="my-2">
-            {/* Descripcion */}
-            <label htmlFor="descripcion" className={classNames(styleLabel)}>
-              Ingrese la descripción
-            </label>
-            <input
-              className={classNames(styleInput)}
-              type="text"
-              placeholder="Por favor ingrese la descripción"
-              {...register("description")}
-            />
-          </div>
-
-          <div className="my-2">
-            {/* Texto del boton */}
-            <label htmlFor="text-btn" className={classNames(styleLabel)}>
-              Ingrese el texto para el botón
-            </label>
-            <input
-              className={classNames(styleInput)}
-              type="text"
-              placeholder="Por favor ingrese el texto del botón"
-              {...register("textButton")}
-            />
-          </div>
-
-          <div className="my-2">
-            {/* Fondo */}
-            <label htmlFor="img" className={classNames(styleLabel)}>
-              Seleccione el fondo
-            </label>
-
-            <ImageUpload onFileChange={handleFileChange} />
-          </div>
-          <button
-            type="submit"
-            className="self-center mt-6 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline"
-          >
-            Enviar
-          </button>
-
-        </form>
+      {/* Renderizar la tabla */}
+      <div className="overflow-x-auto mx-4">
+        <Table config={articlesField} source={callApi} />
       </div>
     </div>
   );
