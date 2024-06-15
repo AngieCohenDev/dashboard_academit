a839da9da0c0afe134feae6948a1c98969a97664
Jimenez Cohen, Francisco Javier (Externos - AcidLabs) <franciscojavier.jimenezcohen@externos-cl.cencosud.com>
2024-06-15 16:22:00 -0300

    add : cambios con componentes reutilizables

--- src/components/HeaderGrid.jsx
+++ src/components/HeaderGrid.jsx
@@ -1,39 +1,87 @@
-import classNames from "classnames";
-import ImageUpload from "../helpers/ImageUpload";
-import { useOnSubmitHeader } from "./hooks/useOnSubmitHeader";
-import { useState } from "react";
-import { Table } from "./shared/Table";
-import axios from "axios";
-import { Input } from "./shared/Input";
+import classNames from 'classnames';
+import ImageUpload from '../helpers/ImageUpload';
+import { useOnSubmitHeader } from './hooks/useOnSubmitHeader';
+import { useEffect, useState } from 'react';
+import { Table } from './shared/Table';
+import { Input } from './shared/Input';
+import { DynamicForm } from './shared/DinamicForm/DynamicForm';
+import { PlusIcon } from '@heroicons/react/24/solid';
+import axios from 'axios';
+import { solicitudFetch } from '../helpers/solicitudesFetch'; // Asegúrate de que esta ruta es correcta
 
 const articlesField = {
-  keys: ["id", "item01", "item02", "item03", "item04", "logo", "createdAt", "updatedAt"],
-  labels: ["Id", "Item01", "Item02", "Item03", "Item04", "Logo", "Creado", "Actualizado"],
+  keys: ['id', 'item01', 'item02', 'item03', 'item04', 'logo', 'createdAt', 'updatedAt'],
+  labels: ['Id', 'Item01', 'Item02', 'Item03', 'Item04', 'Logo', 'Creado', 'Actualizado'],
 };
 
-const styleLabel = "font-medium text-sm py-1 ";
-const styleInput = "w-full h-[40px] px-2 text-slate-400 text-xs my-1 rounded-lg border bg-gray-100";
+const fields = [
+  { id: 'id', label: 'Id', type: 'text', required: false },
+];
 
-const callApi = async () => {
+const callApi = async (page = 1, limit = 5, searchParams = {}) => {
+  const paramsSearch = Object.keys(searchParams)?.reduce((acc, key) => {
+    const label = key;
+    const value = searchParams[key];
+    return acc + `&${label}=${value}`;
+  }, '');
+
   const config = {
-    method: "get",
+    method: 'get',
     maxBodyLength: Infinity,
-    url: "http://localhost:8080/headers",
+    url: `http://localhost:8080/headers?page=${page}&limit=${limit}${paramsSearch}`,
     headers: {
-      Accept: "application/json",
+      Accept: 'application/json',
     },
   };
   const response = await axios.request(config);
-  console.log(JSON.stringify(response.data, null, 4));
+  return response.data;
+};
+
+const updateItem = async (id, data) => {
+  const config = {
+    method: 'patch',
+    url: `http://localhost:8080/headers/${id}`,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    data,
+  };
+  const response = await axios.request(config);
+  return response.data;
+};
+
+const deleteItem = async (id) => {
+  const config = {
+    method: 'delete',
+    url: `http://localhost:8080/headers/${id}`,
+  };
+  const response = await axios.request(config);
   return response.data;
 };
 
 function HeaderGrid() {
-
-  // Hook personalizado para manejar las solicitudes fetch
-  const { handleFileChange, onSubmit, register, inputList, setInputList } = useOnSubmitHeader();
-
+  const { handleFileChange, onSubmit, register, inputList, setInputList, resetForm } = useOnSubmitHeader();
   const [showPopup, setShowPopup] = useState(false);
+  const [data, setData] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentItem, setCurrentItem] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await callApi(currentPage);
+        setData(response.data);
+        setTotalItems(response.pagination.totalItems);
+        setTotalPages(response.pagination.pageCount);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [currentPage]);
 
   const openPopup = () => {
     setShowPopup(true);
@@ -41,6 +89,8 @@ function HeaderGrid() {
 
   const closePopup = () => {
     setShowPopup(false);
+    setCurrentItem(null);
+    resetForm();
   };
 
   const addItem = () => {
@@ -56,48 +106,96 @@ function HeaderGrid() {
     }
   };
 
-  return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      {/* Botón para abrir el popup */}
-      <div className="flex justify-between items-center px-4 py-2 bg-white shadow-md mb-4">
-        <input
-          type="text"
-          className="w-1/3 h-10 px-2 text-slate-400 text-sm my-1 rounded-lg border bg-gray-100"
-          placeholder="Buscar..."
-        />
-        <button
-          onClick={openPopup}
-          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline"
-        >
-          Abrir Formulario
-        </button>
-      </div>
+  const handleFormSubmit = async (formData) => {
+    console.log('Form data submitted:', formData);
+    console.log('Entrando aca:', formData);
+    try {
 
-      {/* Popup */}
+      await solicitudFetch(formData, 'POST', 'headers'); // Lógica para crear un nuevo item
+      const response = await callApi(1, 5);
+      setData(response.data);
+      setTotalItems(response.pagination.totalItems);
+      setTotalPages(response.pagination.pageCount);
+      setCurrentPage(1);
+      //callApi
+      // if (currentItem) {
+      //   await updateItem(currentItem.id, formData);
+      // } else {
+      //   await solicitudFetch(formData, 'POST', 'headers'); // Lógica para crear un nuevo item
+      // }
+  
+    } catch (error) {
+      console.error('Error updating data:', error);
+    }
+    closePopup();
+  };
+
+  const handleEdit = (item) => {
+    setCurrentItem(item);
+    setInputList(fields.map((field, index) => (
+      <Input
+        key={index}
+        number={index + 1}
+        register={register}
+        defaultValue={item[field.id]}
+        name={field.id}
+      />
+    )));
+    openPopup();
+  };
+
+  const handleDelete = async (item) => {
+    console.log('Delete item:', item);
+    try {
+      await deleteItem(item.id);
+      const response = await callApi(currentPage);
+      setData(response.data);
+      setTotalItems(response.pagination.totalItems);
+      setTotalPages(response.pagination.pageCount);
+    } catch (error) {
+      console.error('Error deleting item:', error);
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const extraButtons = [
+    {
+      label: 'Abrir Formulario',
+      onClick: openPopup,
+      className: 'bg-indigo-500 hover:bg-indigo-700',
+      icon: PlusIcon,
+    },
+  ];
+
+  const actions = [
+    { label: 'Editar', onClick: handleEdit },
+    { label: 'Eliminar', onClick: handleDelete },
+  ];
+
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <DynamicForm fields={fields} onSubmit={handleFormSubmit} extraButtons={extraButtons} />
+
       {showPopup && (
         <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full bg-gray-800 bg-opacity-75">
           <div className="bg-white shadow-md rounded-lg p-8 w-2/3">
             <div className="flex justify-between items-center mb-6">
-              <h1 className="text-2xl font-bold">Formulario de Items</h1>
-              <button
-                onClick={closePopup}
-                className="text-gray-600 hover:text-gray-700 focus:outline-none"
-              >
+              <h1 className="text-2xl font-bold">{currentItem ? 'Editar Item' : 'Formulario de Items'}</h1>
+              <button onClick={closePopup} className="text-gray-600 hover:text-gray-700 focus:outline-none">
                 X
               </button>
             </div>
-            <form onSubmit={onSubmit} className="flex flex-col font-sans">
-              
+            <form onSubmit={handleFormSubmit} className="flex flex-col font-sans">
               {inputList}
-
-              {/* Logo */}
               <div className="my-2">
-                <label htmlFor="file" className={classNames(styleLabel)}>
+                <label htmlFor="file" className={classNames('font-medium text-sm py-1')}>
                   Ingrese el logo
                 </label>
                 <ImageUpload onFileChange={handleFileChange} />
               </div>
-
               <div className="mt-8">
                 <div className="flex flex-col items-end space-y-4">
                   <div className="flex space-x-4">
@@ -110,7 +208,6 @@ function HeaderGrid() {
                     </button>
                     <button
                       type="button"
-                      // disabled={inputList.length === 1 ? true : false}
                       onClick={addItem}
                       className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline"
                     >
@@ -129,9 +226,17 @@ function HeaderGrid() {
           </div>
         </div>
       )}
-      {/* Renderizar la tabla */}
+
       <div className="overflow-x-auto mx-4">
-        <Table config={articlesField} source={callApi} />
+        <Table
+          config={articlesField}
+          data={data}
+          totalItems={totalItems}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+          actions={actions}
+        />
       </div>
     </div>
   );
--- src/components/hooks/useOnSubmitHeader.js
+++ src/components/hooks/useOnSubmitHeader.js
@@ -3,59 +3,53 @@ import { solicitudFetch } from "../../helpers/solicitudesFetch";
 import { useState } from "react";
 import { Input } from "../shared/Input";
 
-
 export const useOnSubmitHeader = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const [imageFile, setImageFile] = useState(null);
+  const [inputList, setInputList] = useState([<Input key={0} number={1} register={register} />]);
 
-    const { register, handleSubmit, reset, } = useForm();
+  const handleFileChange = (file) => {
+    setImageFile(file);
+  };
 
-    const [imageFile, setImageFile] = useState(null);
+  const resetForm = () => {
+    reset();
+    setImageFile(null);
+    setInputList([<Input key={0} number={1} register={register} />]);
+  };
 
-    const [inputList, setInputList] = useState([<Input key={0} number={1} register={register} />]);
+  const onSubmit = handleSubmit(async (data) => {
+    const formData = new FormData();
+    const fields = Object.keys(data);
 
-    const handleFileChange = (file) => {
-        setImageFile(file);
-    };
-
-    const onSubmit = handleSubmit(async (data) => {
-
-        const formData = new FormData();
-        const fields = Object.keys(data);
-        //creamos un contador que empieza en cero, esto para determinar si se llamara a POST o PATCH
-
-        // a la variable fields le aplicamos el metodo forEach para ir agregado al formData el nombre de cada 
-        // dato en fields y el value que nos envian desde data
-        fields.forEach(field => {
-            formData.append(field, data[field]);
-        });
-
-        if (imageFile !== null) {
-            console.log(imageFile);
-            formData.append("logo", imageFile);
-            setImageFile(null)
-        }
-
-        // Para ver cada uno de los datos enviados en el form data.
-        for (let pair of formData.entries()) {
-            console.log(`${pair[0]}: ${pair[1]}`);
-        }
-
-        try {
-            await solicitudFetch(formData, 'POST', 'headers');
-            reset();
-            setInputList([<Input key={0} number={1} register={register} />])
-        } catch (error) {
-            console.error("Error:", error);
-        }
-
-
+    fields.forEach((field) => {
+      formData.append(field, data[field]);
     });
 
-    return {
-        handleFileChange,
-        onSubmit,
-        register,
-        inputList,
-        setInputList
+    if (imageFile !== null) {
+      formData.append("logo", imageFile);
+      setImageFile(null);
     }
 
-}
+    // Para ver cada uno de los datos enviados en el form data.
+    for (let pair of formData.entries()) {
+      console.log(`${pair[0]}: ${pair[1]}`);
+    }
+
+    try {
+      await solicitudFetch(formData, 'POST', 'headers');
+      resetForm();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
+
+  return {
+    handleFileChange,
+    onSubmit,
+    register,
+    inputList,
+    setInputList,
+    resetForm
+  };
+};
