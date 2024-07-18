cfb418213ac37b38e783b5b32ae56fd9a573cd6d
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-07-17 22:57:40 -0300

    se cargan en el formulario de edicion los datos del curso y de los videos

--- src/components/shared/Edit/CursosEdit.jsx
+++ src/components/shared/Edit/CursosEdit.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import StepOne from '../StepForm/StepOne'
 import StepTwoPrueba from './StepTwoEdit';
+import axios from 'axios';
+
+
 
 const fields = [
   { type: 'text', name: 'nombreCurso', label: 'Nombre del Curso' },
@@ -31,52 +34,83 @@ const fields = [
   { type: 'file', name: 'fotografiaDelCurso', label: 'Portada del Curso' }
 ];
 
-const videos = [
-  {
-    id: 1,
-    title: "Video 1",
-    fields: [
-      { label: 'Nombre', name: 'name', type: 'text' },
-      { label: 'Duración', name: 'duration', type: 'number' },
-      { label: 'Categoría', name: 'category', type: 'select', options: [{ value: 'cat1', label: 'Categoría 1' }, { value: 'cat2', label: 'Categoría 2' }] },
-      { label: 'Archivo', name: 'file', type: 'file' },
-      { label: 'Miniatura', name: 'file', type: 'file' },
-      { label: 'Material', name: 'button', type: 'button' }
-    ]
-  },
-  {
-    id: 2,
-    title: "Video 2",
-    fields: [
-      { label: 'Nombre', name: 'name', type: 'text' },
-      { label: 'Duración', name: 'duration', type: 'number' },
-      { label: 'Categoría', name: 'category', type: 'select', options: [{ value: 'cat1', label: 'Categoría 1' }, { value: 'cat2', label: 'Categoría 2' }] },
-      { label: 'Archivo', name: 'file', type: 'file' },
-      { label: 'Miniatura', name: 'file', type: 'file' },
-      { label: 'Material', name: 'button', type: 'button' }
-    ]
-  },
-  {
-    id: 3,
-    title: "Video 3",
-    fields: [
-      { label: 'Nombre', name: 'name', type: 'text' },
-      { label: 'Duración', name: 'duration', type: 'number' },
-      { label: 'Categoría', name: 'category', type: 'select', options: [{ value: 'cat1', label: 'Categoría 1' }, { value: 'cat2', label: 'Categoría 2' }] },
-      { label: 'Archivo', name: 'file', type: 'file' },
-      { label: 'Miniatura', name: 'file', type: 'file' },
-      { label: 'Material', name: 'button', type: 'button' }
+// const videos = [
+//   {
+//     id: 1,
+//     title: 'video 1',
+//     fields: [
+//       { label: 'Titulo', name: 'tituloVideo', type: 'text' },
+//       { label: 'Descripcion', name: 'descripcion', type: 'text' },
+//       { label: 'Clase', name: 'clase', type: 'number' },
+//       { label: 'Estatus', name: 'estatus', type: 'select', options: [{ value: true, label: 'Activo' }, { value: false, label: 'Inactivo' }] },
+//       { label: 'Video', name: 'archivoVideo', type: 'file' },
+//       { label: 'Miniatura', name: 'archivoMiniatura', type: 'file' },
+//       { label: 'Material', name: 'button', type: 'button' }
+//     ],
+//   }
+// ]
 
-    ]
-  },
-];
 
 
 export const CursosEdit = () => {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
-    videos: []
+    videos : []
   });
+
+  const callApiCursos = async (id = 1) => {
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: `http://localhost:8080/cursos/${id}`,
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+    try {
+      const response = await axios.request(config);
+      return response.data;
+    } catch (error) {
+      return {
+        type: 'unsucces',
+        message: error.response.data.message
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await callApiCursos(1);
+        setFormData(response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, [])
+
+  const videos = formData.videos.map((video) => (
+    {
+      id: video.idVideo,
+      title: video.tituloVideo,
+      fields: [
+        { label: 'Titulo', name: 'tituloVideo', type: 'text' },
+        { label: 'Descripcion', name: 'descripcion', type: 'text' },
+        { label: 'Clase', name: 'clase', type: 'number' },
+        { label: 'Estatus', name: 'estatus', type: 'select', options: [{ value: true, label: 'Activo' }, { value: false, label: 'Inactivo' }] },
+        { label: 'Video', name: 'archivoVideo', type: 'file' },
+        { label: 'Miniatura', name: 'archivoMiniatura', type: 'file' },
+        { label: 'Material', name: 'button', type: 'button' }
+      ],
+      tituloVideo: video.tituloVideo,
+      descripcion: video.descripcion,
+      clase : video.clase,
+      estatus : video.estatus
+    })
+  );
+
+
 
   const nextStep = () => {
     setStep(step + 1);
@@ -114,7 +148,8 @@ export const CursosEdit = () => {
     <>
       <div className='bg-white shadow-md rounded-lg  '>
         <StepOne title='Editar curso' fields={fields} nextStep={nextStep} handleChange={handleChange} handleFileChange={handleFileChange} formData={formData} showButton={false} />
-        <StepTwoPrueba videos={videos}
+        <StepTwoPrueba
+          videos={videos}
           handleUpdateVideo={handleUpdateVideo}
           prevStep={() => console.log('Anterior')}
           nextStep={() => console.log('Siguiente')} />
